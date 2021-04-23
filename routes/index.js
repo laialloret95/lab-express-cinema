b3c34e367981f1ba38401c5beaabc9981e52abf9
Laia Lloret <laialloret95@gmail.com>
2021-04-23 15:14:50 +0200

    logic working

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
-/* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie.find()
+      .then(moviesFromDB => { // moviesFromDB is an array
+        res.render('movies', {moviesFromDB});
+      })
+      .catch(error => console.log(error));
+})
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId
+    Movie.find({_id: movieId})
+      .then(movie => { // moviesFromDB is an array
+        res.render('movie-detail', {movie});
+      })
+      .catch(error => console.log(error));
+})
 
 module.exports = router;
