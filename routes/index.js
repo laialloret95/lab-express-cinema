bde0224985e9726984061a0ed1da001a08142b08
Laia Lloret <laialloret95@gmail.com>
2021-04-23 15:27:32 +0200

    apply order and style

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => res.render('index'));
 router.get('/movies', (req, res) => {
     Movie.find()
       .then(moviesFromDB => { // moviesFromDB is an array
-        res.render('movies', {moviesFromDB});
+        res.render('movies/movies', {moviesFromDB});
       })
       .catch(error => console.log(error));
 })
@@ -16,7 +16,7 @@ router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId
     Movie.find({_id: movieId})
       .then(movie => { // moviesFromDB is an array
-        res.render('movie-detail', {movie});
+        res.render('movies/movie-detail', {movie});
       })
       .catch(error => console.log(error));
 })
